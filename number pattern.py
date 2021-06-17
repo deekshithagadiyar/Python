84df09dc736361103f5cc116132ad519f2fa70bd
deekshithagadiyar <69233278+deekshithagadiyar@users.noreply.github.com>
2021-06-17 11:37:04 +0530

    Update number pattern.py

--- number pattern.py
+++ number pattern.py
@@ -1,3 +1,22 @@
+'''
+Problem 6:
+PRINT THE BELOW MENTIONED PATTERN FOR ANY "N" VALUE. WHERE "N" INDICATES NO.OF ROWS.
+Input Format
+A SINGLE INTEGER DENOTING N VALUE
+Constraints
+1<=N<=100
+Output Format
+PATTERN AS SHOWN IN SAMPLE TEST CASE
+Sample Input 0
+5
+Sample Output 0
+1
+ 2
+  3
+   4
+    5
+'''
+
 n=int(input())
 for index in range(n):
     print(' '*index,(index+1))
