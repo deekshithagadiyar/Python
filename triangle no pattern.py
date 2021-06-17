6be43d111ed5d4e29f1d2e95ac3cee1f0f6a386d
deekshithagadiyar <69233278+deekshithagadiyar@users.noreply.github.com>
2021-06-17 11:44:11 +0530

    Update triangle no pattern.py

--- triangle no pattern.py
+++ triangle no pattern.py
@@ -1,3 +1,10 @@
+'''
+program to print the following pattern
+       1
+     2 1 2
+   3 2 1 2 3
+ 4 3 2 1 2 3 4
+'''
 n=int(input())
 for i in range(1,n+1):
     print(' '*(n-i),*range(i,1,-1),*range(1,i+1),sep='')
