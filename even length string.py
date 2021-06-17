7da332a78c6affca53ee59ab0fb556f2f25af93a
deekshithagadiyar <69233278+deekshithagadiyar@users.noreply.github.com>
2021-06-17 11:32:37 +0530

    Update even length string.py

--- even length string.py
+++ even length string.py
@@ -1,3 +1,17 @@
+'''
+Problem 9:
+Python program to print even length words in a string
+Given a string. The task is to print all words with even length in the given string.
+Examples:
+Input: s = "This is a python language"
+Output: This
+        is
+        python
+        language
+Input: s = "i am Dlithe"
+Output: am
+        Dlithe
+'''
 s=input('s=')
 s = s.split(' ')
 for word in s:
