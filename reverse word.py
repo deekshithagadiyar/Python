314095f81f77824e9a4e5afa1073bf76f90fb3a5
deekshithagadiyar <69233278+deekshithagadiyar@users.noreply.github.com>
2021-06-17 11:39:46 +0530

    Update reverse word.py

--- reverse word.py
+++ reverse word.py
@@ -1,3 +1,11 @@
+'''
+Problem 7:
+Reverse words in a given String in Python
+We are given a string and we need to reverse words of given string ?
+Examples:
+Input : str = "think career in IT think dlithe"
+Output : str = "dlithe think IT in career think"
+'''
 s=input("str=")
 words = s.split()
 words = list(reversed(words))
